2a1094fa09ccd0fd56cfc0ca3dd2fba507902e17
namelf <namelf@DESKTOP-EV1NID7>
2024-10-03 00:35:30 +0700

    test

---
+++ WebApplication1/CaptchaGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+public class CaptchaGenerator
+{
+    public static void Main()
+    {
+        string captchaText = GenerateRandomText(6);
+        Bitmap captchaImage = GenerateCaptchaImage(captchaText);
+
+        // Save the image to a file
+        captchaImage.Save("captcha.jpg", ImageFormat.Jpeg);
+
+        Console.WriteLine("CAPTCHA generated and saved as captcha.jpg");
+    }
+
+    private static string GenerateRandomText(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random = new Random();
+        char[] result = new char[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = chars[random.Next(chars.Length)];
+        }
+
+        return new string(result);
+    }
+
+    private static Bitmap GenerateCaptchaImage(string captchaText)
+    {
+        Bitmap bitmap = new Bitmap(200, 50);
+        Graphics graphics = Graphics.FromImage(bitmap);
+        Font font = new Font("Arial", 20, FontStyle.Bold);
+        Rectangle rect = new Rectangle(0, 0, 200, 50);
+
+        // Fill the background with a color
+        graphics.Clear(Color.White);
+
+        // Draw the CAPTCHA text
+        graphics.DrawString(captchaText, font, Brushes.Black, rect);
+
+        // Add some noise
+        Random random = new Random();
+        for (int i = 0; i < 1000; i++)
+        {
+            int x = random.Next(200);
+            int y = random.Next(50);
+            bitmap.SetPixel(x, y, Color.Gray);
+        }
+
+        return bitmap;
+    }
+}
---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private CaptchaGenerator captcha;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GenerateCaptcha();
+            }
+        }
+        private void GenerateCaptcha()
+        {
+            captcha = new CaptchaGenerator();
+            Session["CaptchaText"] = captcha.CaptchaText;
+            imgCaptcha.ImageUrl = "data:image/jpeg;base64," + Convert.ToBase64String(captcha.GenerateCaptchaImage());
+        }
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtCaptcha.Text != Session["CaptchaText"].ToString())
+            {
+                lblMessage.Text = "Invalid CAPTCHA.";
+                GenerateCaptcha();
+                return;
+            }
+
+            string connectionString = ConfigurationManager.ConnectionStrings["YourConnectionString"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+
+                int count = (int)cmd.ExecuteScalar();
+                if (count > 0)
+                {
+                    lblMessage.ForeColor = System.Drawing.Color.Green;
+                    lblMessage.Text = "Login successful!";
+                }
+                else
+                {
+                    lblMessage.Text = "Invalid username or password.";
+                }
+            }
+        }
+    }
+
+}
