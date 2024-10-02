006e5af91dce2862ca44686199d0f90218e15bfc
namelf <namelf@DESKTOP-EV1NID7>
2024-10-02 22:50:51 +0700

    TEST

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button validateButton;
+
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.validateButton = new System.Windows.Forms.Button();
+            this.refreshButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // validateButton
+            // 
+            this.validateButton.Location = new System.Drawing.Point(12, 94);
+            this.validateButton.Name = "validateButton";
+            this.validateButton.Size = new System.Drawing.Size(200, 23);
+            this.validateButton.TabIndex = 2;
+            this.validateButton.Text = "Validate CAPTCHA";
+            this.validateButton.UseVisualStyleBackColor = true;
+            this.validateButton.Click += new System.EventHandler(this.validateButton_Click);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(12, 123);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(200, 23);
+            this.refreshButton.TabIndex = 3;
+            this.refreshButton.Text = "Refresh CAPTCHA";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.validateButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "CAPTCHA Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button refreshButton;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private string captchaText;
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateAndDisplayCaptcha();
+        }
+
+        private void GenerateAndDisplayCaptcha()
+        {
+            string captchaText = GenerateRandomText(6);
+            Bitmap captchaImage = GenerateCaptchaImage(captchaText);
+
+            // Display the image in the PictureBox
+            pictureBox1.Image = captchaImage;
+        }
+
+        private string GenerateRandomText(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            char[] result = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new string(result);
+        }
+
+        private Bitmap GenerateCaptchaImage(string captchaText)
+        {
+            Bitmap bitmap = new Bitmap(200, 50);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Font font = new Font("Arial", 20, FontStyle.Bold);
+            Rectangle rect = new Rectangle(0, 0, 200, 50);
+
+            // Fill the background with a color
+            graphics.Clear(Color.White);
+
+            // Draw the CAPTCHA text
+            graphics.DrawString(captchaText, font, Brushes.Black, rect);
+
+            // Add some noise
+            Random random = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                int x = random.Next(200);
+                int y = random.Next(50);
+                bitmap.SetPixel(x, y, Color.Gray);
+            }
+
+            return bitmap;
+        }
+        private void validateButton_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == captchaText)
+            {
+                MessageBox.Show("CAPTCHA validation successful!");
+            }
+            else
+            {
+                MessageBox.Show("CAPTCHA validation failed. Please try again.");
+            }
+            
+        }
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            GenerateAndDisplayCaptcha();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
